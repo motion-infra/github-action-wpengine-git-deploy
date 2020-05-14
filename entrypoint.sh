5d2c14cee61af790b4779596c790c81e70e8b160
Eric Willis <eric@ecwillis.com>
2020-05-14 18:05:40 +0000

    try the one thing.

--- entrypoint.sh
+++ entrypoint.sh
@@ -30,4 +30,5 @@ chmod 644 "$WPENGINE_SSH_KEY_PUBLIC_PATH"
 
 git config core.sshCommand "ssh -i $WPENGINE_SSH_KEY_PRIVATE_PATH -o UserKnownHostsFile=$KNOWN_HOSTS_PATH"
 git remote add $WPENGINE_ENV git@$WPENGINE_HOST:$WPENGINE_ENV/$WPENGINE_ENVIRONMENT_NAME.git
+git fetch --unshallow $WPENGINE_ENV
 git push -f $WPENGINE_ENV $BRANCH:master
